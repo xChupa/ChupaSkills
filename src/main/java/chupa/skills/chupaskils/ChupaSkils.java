a1cab4fb7ac30f2907fec4b122a340559d8f07da
FatSaw <eklesosek@gmail.com>
2023-05-20 03:14:20 +0300

    Update ChupaSkils.java

--- src/main/java/chupa/skills/chupaskils/ChupaSkils.java
+++ src/main/java/chupa/skills/chupaskils/ChupaSkils.java
@@ -2,27 +2,22 @@ package chupa.skills.chupaskils;
 
 import java.io.File;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class ChupaSkils extends JavaPlugin {
 
-	private static ChupaSkils instance;
+	protected static int start_price_xp = 0;
 
     @Override
     public void onEnable() {
-        instance = this;
         saveDefaultConfig();
-
+        start_price_xp = getConfig().getInt("start_price_exp",0);
         getCommand("skill").setExecutor(new SkillCommand());
         getServer().getPluginManager().registerEvents(new SkillGui(), this);
         getServer().getPluginManager().registerEvents(new SkillData(new File(getDataFolder(),"players.yml")), this);
         getServer().getPluginManager().registerEvents(new DamageHandler(), this);
         
         LangOptions.values();
-    }
-    protected static FileConfiguration config() {
-    	return instance.getConfig();
     }
 }
